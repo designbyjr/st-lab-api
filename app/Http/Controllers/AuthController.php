ae25aac558b5cf159207293188ba49e6658837f2
Jamie Design BY JR <one4allplugin@gmail.com>
2023-07-25 14:33:45 +0100

    Added New store user request class and amended the current AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponse;
+
+    public function login()
+    {
+        return "Login Success";
+    }
+
+    public function logout()
+    {
+        return "Logout Success";
+    }
+
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated();
+
+        $user = User::create([
+            'name' => $request->name,
+            'email ' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return $this->success([
+            'user' => $user,
+            'bearer_token' => $user->createToken('API Token Of '. $user->name)->plainTextToken
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post("/login",[AuthController::class, 'login']);
 Route::post("/logout",[AuthController::class, 'logout']);
 Route::post("/register",[AuthController::class, 'register']);
+
+Route::resource('/products', ProductsController::class);
